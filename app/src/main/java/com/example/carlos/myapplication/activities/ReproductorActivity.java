62c03599232090f3039c89f1cecff419a5075a7b
RDR96 <00231514@uca.edu.sv>
2018-07-07 20:48:54 -0600

    Se agrego pantalla de reproduccion

---
+++ app/src/main/java/com/example/carlos/myapplication/activities/ReproductorActivity.java
@@ -0,0 +1,15 @@
+package com.example.carlos.myapplication.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.carlos.myapplication.R;
+
+public class ReproductorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reproductor);
+    }
+}
---
+++ app/src/main/java/com/example/carlos/myapplication/objects/Helpers.java
@@ -0,0 +1,17 @@
+package com.example.carlos.myapplication.objects;
+
+import android.media.MediaPlayer;
+
+/**
+ * Created by Rodrigo Corvera on 7/7/2018.
+ */
+
+public class Helpers {
+
+    MediaPlayer mediaPlayer;
+
+    public static void getMediaPlayer () {
+
+    }
+
+}
