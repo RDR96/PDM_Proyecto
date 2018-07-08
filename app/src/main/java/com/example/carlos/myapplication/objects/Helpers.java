699b89bcefa892eeb815ae7c15b79eec9856a176
RDR96 <00231514@uca.edu.sv>
2018-07-08 02:54:57 -0600

    Se listan las canciones del telefono, ademas se añadio la funcion de reproducir la cancion en una ventana aparte

--- app/src/main/java/com/example/carlos/myapplication/objects/Helpers.java
+++ app/src/main/java/com/example/carlos/myapplication/objects/Helpers.java
@@ -1,6 +1,9 @@
 package com.example.carlos.myapplication.objects;
 
+import android.content.Context;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 
 /**
  * Created by Rodrigo Corvera on 7/7/2018.
@@ -8,10 +11,29 @@ import android.media.MediaPlayer;
 
 public class Helpers {
 
-    MediaPlayer mediaPlayer;
+    static MediaPlayer mediaPlayer;
 
-    public static void getMediaPlayer () {
+    public static MediaPlayer getPlayer(Context context, String path) {
 
+        if(mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(context, Uri.parse(path));
+        }else{
+            mediaPlayer.stop();
+            mediaPlayer = MediaPlayer.create(context, Uri.parse(path));
+        }
+
+        return mediaPlayer;
     }
+
+
+    public static boolean checkVersion5_1(){
+        double version = java.lang.Double.parseDouble(new String(Build.VERSION.RELEASE).replaceAll("(\\d+[.]\\d+)(.*)", "$1"));
+        if (version >= 5.1) {
+            return true;
+        } else{
+            return false;
+        }
+    }
+
 
 }
