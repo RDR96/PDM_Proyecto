f21130cceb76e3da0c076b5525d13085dcb37a3e
Didier Cruz <didier.cruz98@gmail.com>
2018-07-10 01:01:14 -0600

    pantallas animadas

--- app/src/main/java/com/example/carlos/myapplication/activities/MisCancionesActivity.java
+++ app/src/main/java/com/example/carlos/myapplication/activities/MisCancionesActivity.java
@@ -1,8 +1,11 @@
 package com.example.carlos.myapplication.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.example.carlos.myapplication.R;
 
@@ -12,5 +15,18 @@ public class MisCancionesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_canciones);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/carlos/myapplication/fragments/PerfilFragment.java
+++ app/src/main/java/com/example/carlos/myapplication/fragments/PerfilFragment.java
@@ -8,8 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.widget.CardView;
 
-import com.example.carlos.myapplication.activities.ArtistasActivity;
 import com.example.carlos.myapplication.activities.AlbumesActivity;
+import com.example.carlos.myapplication.activities.ArtistasActivity;
 import com.example.carlos.myapplication.activities.CancionesFavoritasActivity;
 import com.example.carlos.myapplication.activities.MisCancionesActivity;
 import com.example.carlos.myapplication.activities.PlaylistActivity;
